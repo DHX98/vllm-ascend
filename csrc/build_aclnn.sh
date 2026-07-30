a63d5e1d68a562aa51163b9e8e28080433e4375b
DHX98 <haoxiand@andrew.cmu.edu>
2026-07-30 15:00:28 +0800

    fix(ops): build chunk GDN op on Ascend 910B

--- csrc/build_aclnn.sh
+++ csrc/build_aclnn.sh
@@ -133,6 +133,7 @@ elif [[ "$SOC_VERSION" =~ ^ascend910b ]]; then
         "ngram_spec_decode"
         "chunk_fwd_o"
         "chunk_gated_delta_rule_fwd_h"
+        "chunk_gated_delta_rule"
         "store_kv_block"
     )
 
